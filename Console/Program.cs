080c37b639f68f0f0759d9f46aeeb3c73badffdb
Mark van der Spoel <mark.vanderspoel@sunwebgroup.com>
2022-01-31 10:14:14 +0100

    Add outdated nuget package

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Drawing;
+using Console = Colorful.Console;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!", Color.Blue);
